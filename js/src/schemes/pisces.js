d1fc5df3921388d9f0ca275a47fa14ba3ba5a6e5
wudi.viseator <wudi.viseator@bytedance.com>
2019-01-16 14:26:03 +0800

    Site updated: 2019-01-16 14:26:03

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -3,40 +3,51 @@
 $(document).ready(function() {
 
   var sidebarInner = $('.sidebar-inner');
-  var sidebarOffset = CONFIG.sidebar.offset ? CONFIG.sidebar.offset : 12;
+  var sidebarOffset = CONFIG.sidebar.offset || 12;
 
   function getHeaderOffset() {
     return $('.header-inner').height() + sidebarOffset;
   }
 
   function getFooterOffset() {
+    var footer = $('#footer');
     var footerInner = $('.footer-inner');
-    var footerMargin = footerInner.outerHeight(true) - footerInner.outerHeight();
-    var footerOffset = footerInner.outerHeight(true) + footerMargin;
+    var footerMargin = footer.outerHeight() - footerInner.outerHeight();
+    var footerOffset = footer.outerHeight() + footerMargin;
     return footerOffset;
   }
 
   function initAffix() {
     var headerOffset = getHeaderOffset();
     var footerOffset = getFooterOffset();
+    var sidebarHeight = $('#sidebar').height() + NexT.utils.getSidebarb2tHeight();
+    var contentHeight = $('#content').height();
 
-    sidebarInner.affix({
-      offset: {
-        top   : headerOffset - sidebarOffset,
-        bottom: footerOffset
-      }
-    });
+    // Not affix if sidebar taller than content (to prevent bottom jumping).
+    if (headerOffset + sidebarHeight < contentHeight) {
+      sidebarInner.affix({
+        offset: {
+          top   : headerOffset - sidebarOffset,
+          bottom: footerOffset
+        }
+      });
+      sidebarInner.affix('checkPosition');
+    }
 
-    $('#sidebar').css({ 'margin-left': 'initial', 'margin-top': headerOffset });
-    sidebarInner.affix('checkPosition');
+    $('#sidebar').css({ 'margin-top': headerOffset, 'margin-left': 'auto' });
+  }
+
+  function recalculateAffixPosition() {
+    $(window).off('.affix');
+    sidebarInner.removeData('bs.affix').removeClass('affix affix-top affix-bottom');
+    initAffix();
   }
 
   function resizeListener() {
-    var mql = window.matchMedia('(min-width: 991px)');
+    var mql = window.matchMedia('(min-width: 992px)');
     mql.addListener(function(e) {
       if (e.matches) {
-        sidebarInner.affix('checkPosition');
-        initAffix();
+        recalculateAffixPosition();
       }
     });
   }
--- js/src/utils.js
+++ js/src/utils.js
@@ -7,7 +7,7 @@ NexT.utils = NexT.$u = {
    */
   wrapImageWithFancyBox: function() {
     $('.content img')
-      .not('[hidden]')
+      .not(':hidden')
       .not('.group-picture img, .post-gallery img')
       .each(function() {
         var $image = $(this);
@@ -17,10 +17,9 @@ NexT.utils = NexT.$u = {
         if ($imageWrapLink.length < 1) {
           var imageLink = $image.attr('data-original') ? this.getAttribute('data-original') : this.getAttribute('src');
           $imageWrapLink = $image.wrap('<a data-fancybox="group" href="' + imageLink + '"></a>').parent('a');
+          $imageWrapLink.addClass('fancybox fancybox.image');
+          $imageWrapLink.attr('rel', 'group');
         }
-
-        $imageWrapLink.addClass('fancybox fancybox.image');
-        $imageWrapLink.attr('rel', 'group');
 
         if (imageTitle) {
           $imageWrapLink.append('<p class="image-caption">' + imageTitle + '</p>');
@@ -57,7 +56,7 @@ NexT.utils = NexT.$u = {
     $(function() {
       $(window).bind('hashchange', function() {
         var tHash = location.hash;
-        if (tHash !== '') {
+        if (tHash !== '' && !tHash.match(/%\S{2}/)) {
           $(tNav + 'li:has(a[href="' + tHash + '"])').addClass('active').siblings().removeClass('active');
           $(tHash).addClass('active').siblings().removeClass('active');
         }
@@ -80,7 +79,6 @@ NexT.utils = NexT.$u = {
         }
       }
     });
-
   },
 
   registerESCKeyEvent: function() {
@@ -110,8 +108,8 @@ NexT.utils = NexT.$u = {
       $('#scrollpercent>span').html(scrollPercentMaxed);
     }
 
-    // For init back to top in sidebar if page was already scrolled.
-    $(document).ready(function() {
+    // For init back to top in sidebar if page was scrolled after page refresh.
+    $(window).on('load', function() {
       initBackToTop();
     });
 
@@ -120,7 +118,7 @@ NexT.utils = NexT.$u = {
     });
 
     $top.on('click', function() {
-      $('body').velocity('scroll');
+      $.isFunction($('html').velocity) ? $('body').velocity('scroll') : $('html, body').animate({ scrollTop: 0 });
     });
   },
 
@@ -241,6 +239,10 @@ NexT.utils = NexT.$u = {
     $('.sidebar-toggle').trigger('click');
   },
 
+  isMuse: function() {
+    return CONFIG.scheme === 'Muse';
+  },
+
   isMist: function() {
     return CONFIG.scheme === 'Mist';
   },
@@ -257,7 +259,6 @@ NexT.utils = NexT.$u = {
     var $div = $('<div />').addClass('scrollbar-measure').prependTo('body');
     var div = $div[0];
     var scrollbarWidth = div.offsetWidth - div.clientWidth;
-
     $div.remove();
 
     return scrollbarWidth;
@@ -280,12 +281,12 @@ NexT.utils = NexT.$u = {
     var sidebarNavHeight = $('.sidebar-nav').css('display') === 'block' ? $('.sidebar-nav').outerHeight(true) : 0;
     var sidebarInner = $('.sidebar-inner');
     var sidebarPadding = sidebarInner.innerWidth() - sidebarInner.width();
+    var sidebarOffset = CONFIG.sidebar.offset ? CONFIG.sidebar.offset : 12;
     var sidebarSchemePadding = this.isPisces() || this.isGemini()
-      ? (sidebarPadding * 2) + sidebarNavHeight + (CONFIG.sidebar.offset * 2) + this.getSidebarb2tHeight()
+      ? (sidebarPadding * 2) + sidebarNavHeight + sidebarOffset + this.getSidebarb2tHeight()
       : (sidebarPadding * 2) + (sidebarNavHeight / 2);
     return sidebarSchemePadding;
   }
-
 };
 
 $(document).ready(function() {
@@ -325,7 +326,5 @@ $(document).ready(function() {
     // Initialize Sidebar & TOC Height.
     updateSidebarHeight(document.body.clientHeight - NexT.utils.getSidebarSchemePadding());
   }
-
   initSidebarDimension();
-
 });
